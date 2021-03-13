55f20c4d34389cfeec7ee19b1b63fee87a74a0f6
Roferhn <58787561+Roferhn@users.noreply.github.com>
2021-03-12 23:56:16 -0600

    Creacion login.java Cronstructores, Setters y Getters

--- Civerlac/src/civerlac/Login.java
+++ Civerlac/src/civerlac/Login.java
@@ -7,4 +7,42 @@ package civerlac;
  */
 public class Login {
     
+    private String user;
+    private String pass;
+    private String status;
+
+    public Login() {
+    }
+
+    public Login(String user, String pass, String status) {
+        this.user = user;
+        this.pass = pass;
+        this.status = status;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+    
+    
 }
