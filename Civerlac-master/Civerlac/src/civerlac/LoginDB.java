4907bf720fb0bdb0ccf8842ee69585d0df63bd91
Jchacon11 <211Josek@gmail.com>
2021-03-22 00:03:28 -0600

    rama validaciones para registro de clientes

---
+++ Civerlac-master/Civerlac/src/civerlac/LoginDB.java
@@ -0,0 +1,37 @@
+
+package civerlac;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class LoginDB {
+    
+    Connection con = null;
+    PreparedStatement ps= null;
+    ResultSet rs= null;
+    ConexionSQL cn = new ConexionSQL();
+    
+    public Login log(String user, String pass){
+        
+        Login l = new Login();
+        String sql = "SELECT * FROM usuarios WHERE idEmpleado = ? AND contrasena = ? AND Estado = 'Habilitado'";
+        
+        try{
+            con = cn.getConnection();
+            ps = con.prepareStatement(sql);//EN ESTA LINEA MUERE. ERROR = "Exception in thread "AWT-EventQueue-0" java.lang.NullPointerException
+            ps.setString(1, user);
+            ps.setString(2, pass);
+            rs = ps.executeQuery();
+            
+            if(rs.next()){
+                l.setUser(rs.getString("idEmpleado"));
+                l.setPass(rs.getString("contrasena"));
+            } 
+        }catch(SQLException e){
+            System.out.println(e.toString());
+        }
+        return l;
+    }
+}
